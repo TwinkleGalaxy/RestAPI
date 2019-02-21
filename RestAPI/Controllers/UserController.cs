66f338b12cb9aa68474d06c0f48ab8b3675b521a
Merna <merna_muawad@hotmail.com>
2019-02-20 22:24:51 -0800

    First commit

---
+++ RestAPI/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET api/user
+        public IEnumerable<UserInfo> Get()
+        {
+            var userInfoList = new List<UserInfo>();
+
+            for(var i = 1; i <= 5; i++)
+            {
+                var userInfo = new UserInfo
+                {
+                    RegisteredUser = $"user #{i}",
+                    DomainItems = "userInfo",
+                    RegisteredDate = DateTime.Now,
+                    ExpiredDate = DateTime.Now.AddMonths(12),
+                    Description = $"user #{i} information"
+                };
+                userInfoList.Add(userInfo);
+            }
+            return userInfoList;
+        }
+
+        // GET api/user/1
+        public UserInfo Get(int id)
+        {
+            return new UserInfo
+            {
+                RegisteredUser = $"user #{id}",
+                DomainItems = "userInfo",
+                RegisteredDate = DateTime.Now.ToUniversalTime(),
+                ExpiredDate = DateTime.Now.ToUniversalTime().AddMonths(12),
+                Description = $"user #{id} information"
+            };
+        }
+    }
+}
---
+++ RestAPI/Models/UserInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RestAPI.Models
+{
+    [DataContract]
+    public class UserInfo
+    {
+        [DataMember(Name = "registeredUser")]
+        public string RegisteredUser { get; set; }
+
+        [DataMember(Name = "domainItems")]
+        public string DomainItems { get; set; }
+
+        [DataMember(Name = "registeredDate")]
+        public DateTime RegisteredDate { get; set; }
+
+        [DataMember(Name = "expiredDate")]
+        public DateTime ExpiredDate { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+    }
+}
